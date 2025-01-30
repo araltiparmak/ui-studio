123973aec9b6e9bd5a05098458f5f0d5da84d5ef
ali <aliriza_altiparmak@hotmail.com>
2025-01-30 16:22:09 +0100

    create ecr

---
+++ terraform/config.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.84.0"
+    }
+  }
+  backend "s3" {
+    bucket = "my-infra"
+    key    = "synapse.tfstate"
+    region = "eu-central-1"
+  }
+  required_version = ">= 1.10.4"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+  default_tags {
+    tags = {
+      ManagedBy = "Terraform"
+      Repo      = local.repo
+    }
+  }
+}
+
+locals {
+  github_account = "araltiparmak"
+  repo           = "synapse"
+}
