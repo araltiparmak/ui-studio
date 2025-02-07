5c1c7484d4b503d922a19c2be784ab0416574898
Ali Rıza Altıparmak <5054445+araltiparmak@users.noreply.github.com>
2025-02-07 21:58:48 +0100

    use oidc module (#13)

--- terraform/oidc.tf
+++ terraform/oidc.tf
@@ -1,31 +1,5 @@
-data "aws_iam_openid_connect_provider" "this" {
-  url = "https://token.actions.githubusercontent.com"
-}
-
-data "aws_iam_policy_document" "oidc" {
-  statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-
-    principals {
-      type        = "Federated"
-      identifiers = [data.aws_iam_openid_connect_provider.this.arn]
-    }
-
-    condition {
-      test     = "StringEquals"
-      values   = ["sts.amazonaws.com"]
-      variable = "token.actions.githubusercontent.com:aud"
-    }
-
-    condition {
-      test     = "StringLike"
-      values   = ["repo:${local.github_account}/${local.repo}:ref:refs/heads/main"]
-      variable = "token.actions.githubusercontent.com:sub"
-    }
-  }
-}
-
-resource "aws_iam_role" "this" {
-  name               = "github_oidc_role_${local.repo}"
-  assume_role_policy = data.aws_iam_policy_document.oidc.json
+module "oidc" {
+  source         = "github.com/araltiparmak/terraform-modules//github-oidc?ref=v1.0.2"
+  github_account = local.github_account
+  repo           = local.repo
 }
