766808ca033e5ce473e5e4a218884f94fbe13c54
ali <aliriza_altiparmak@hotmail.com>
2025-03-22 00:35:22 +0100

    put json to db

--- terraform/website.tf
+++ terraform/website.tf
@@ -3,5 +3,17 @@ module "website" {
   domain_name         = local.bucket
   with_cloudfront     = true
   cloudfront_aliases  = ["ui.araltiparmak.com"]
-  acm_certificate_arn = "arn:aws:acm:us-east-1:789798665829:certificate/e0fdbb0d-d6a5-4e64-88ba-46a39bf7d91e"
+  acm_certificate_arn = local.acm_certificate_arn
+}
+
+data "aws_secretsmanager_secret" "acm_cert" {
+  name = "cloudfront_cert"
+}
+
+data "aws_secretsmanager_secret_version" "acm_cert_version" {
+  secret_id = data.aws_secretsmanager_secret.acm_cert.id
+}
+
+locals {
+  acm_certificate_arn = jsondecode(data.aws_secretsmanager_secret_version.acm_cert_version.secret_string)["acm_certificate_arn"]
 }
